3ed2cf6cc3936898070a833c985914b6ffae3eef
shreyas21 <44678673+VoW314@users.noreply.github.com>
2026-03-19 16:17:08 -0400

    Add files via upload

--- Cats-Bera/src/main/java/edu/vt/controllers/CatApiController.java
+++ Cats-Bera/src/main/java/edu/vt/controllers/CatApiController.java
@@ -9,7 +9,9 @@ import edu.vt.pojo.CatApi;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
+import java.io.BufferedReader;
 import java.io.Serializable;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +20,9 @@ import java.net.URL;
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
+import org.primefaces.shaded.json.JSONObject;
 
-@Named("apiController")
+@Named("CatApiController")
 @SessionScoped
 
 
@@ -28,6 +31,11 @@ import jakarta.json.JsonObject;
 
        public String getCatRandomPhotoUrl(){
            return catRandomPhotoUrl;
+       }
+       private String catFact;
+
+       public String getCatFact(){
+           return catFact;
        }
 
        public void getRandomCatPhoto(){
@@ -46,6 +54,31 @@ import jakarta.json.JsonObject;
                e.printStackTrace();
            }
        }
+
+       /*
+       The below code gets the random cat fact. Since the url will just
+       return a JSON object, this will parse the JSON file to get the fact and
+       will return it to the webpage
+        */
+       public void getRandomCatFact() {
+           try {
+               // the url of the cat fact
+               URL url = new URL("https://catfact.ninja/fact");
+
+               JsonObject jsonObject = Json.createReader(
+                       new InputStreamReader(url.openStream())
+               ).readObject();
+
+               catFact = jsonObject.getString("fact");
+
+               // in the case we can't get this cat fact
+           } catch (Exception e) {
+               e.printStackTrace();
+               catFact = "Unable to get a random cat fact";
+           }
+       }
+
+
    }
 
 
